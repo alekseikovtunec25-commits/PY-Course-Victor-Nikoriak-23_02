f7831f92b13024bdecdc36c00c1a4ad466832611
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-03-22 00:28:08 +0200

    выполнение Task2

--- assignments/homework_08/homework_08.py
+++ assignments/homework_08/homework_08.py
@@ -1,3 +1,15 @@
+#task1
 def favorit_muvie(muvie):
     print(f'Мой любимый фильм называется {muvie}')
 favorit_muvie('Логан')
+
+#task2
+def make_country(name, capital):
+    country={}
+    country['name'] = name
+    country['capital'] = capital
+    return country
+country = make_country('Ukraine', 'Kiyv')
+print(country)
+print(country['name'])
+print(country['capital'])
