700295166af0518c23e21c356dd9116f07fd6021
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-03-22 01:02:18 +0200

    выполнение Task3

--- assignments/homework_08/homework_08.py
+++ assignments/homework_08/homework_08.py
@@ -13,3 +13,26 @@ country = make_country('Ukraine', 'Kiyv')
 print(country)
 print(country['name'])
 print(country['capital'])
+
+#task3
+def make_operation(operator , *args ):
+    if operator =='+':
+        result = 0
+        for args in args:
+            result += args
+        return result
+    elif operator =='-':
+        resultt = 0
+        for args in args:
+            resultt -= args
+        return resultt
+    elif operator =='*':
+        resulttt = 1
+        for args in args:
+            resulttt *= args
+        return resulttt
+result = make_operation('+',3,4,7,9)
+resultt = make_operation('-',5,4,4,2)
+resulttt = make_operation('*',5,4,8)
+
+print(result,resultt,resulttt)
