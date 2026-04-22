39b3c26fdaa1c5c0e78e81ddd1ece0f9edc1545e
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-04-22 19:02:49 +0300

    создание нового модуля и выполнение его импорта "Выполнение таск 2"

--- assignments/Homework_09/homework_09.py
+++ assignments/Homework_09/homework_09.py
@@ -4,3 +4,12 @@ from function_homework import say_hello
 say_hello("Alex")
 say_hello("Bob")
 
+#Task2
+import sys
+print(sys.path)
+
+import sys
+sys.path.append(r"C:\\Users\\aleks\\PycharmProjects\\PY-Course-Victor-Nikoriak-23_02\\assignments\\Homework_09\my_module")
+
+import my_module
+my_module.hello()
