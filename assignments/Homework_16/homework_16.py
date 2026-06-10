1e713e56e09f4d8f22f75b7561912c9366e13ef9
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-06-10 17:05:33 +0300

    Выполнение домашнего задания №16 Task 2

--- assignments/Homework_16/homework_16.py
+++ assignments/Homework_16/homework_16.py
@@ -53,7 +53,7 @@ student = Student("Иван", "Петров", 15, 9)
 student.add_mark(5)
 student.add_mark(4)
 
-teacher = Teacher("Анна", "Сидорова", 40, "Математика", 16000)
+teacher = Teacher("Анна", "Сидорова", 40, "Математику", 16000)
 
 print(student.introduce())
 print(student.study())
@@ -62,3 +62,31 @@ print("Средняя оценка:", student.average_mark())
 print(teacher.introduce())
 print(teacher.teach())
 print("Зарплата:", teacher.get_salary())
+
+#Task 2
+
+class Mathematician:
+
+    def square_nums(self, nums: list):
+        return [num ** 2 for num in nums]
+
+    def remove_positives(self, nums: list):
+        return [num for num in nums if num <= 0]
+
+    def filter_leaps(self, years: list):
+        result = []
+        for year in years:
+            if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+                result.append(year)
+        return result
+
+
+# ===== Проверка =====
+
+m = Mathematician()
+
+assert m.square_nums([7, 11, 5, 4]) == [49, 121, 25, 16]
+assert m.remove_positives([26, -11, -8, 13, -90]) == [-11, -8, -90]
+assert m.filter_leaps([2001, 1884, 1995, 2003, 2020]) == [1884, 2020]
+
+print("Все тесты пройдены!")
