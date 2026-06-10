bd0248b0d3910583d131b3311e9643ffb93f8877
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-06-10 15:47:50 +0300

    выполнение домашнего задания №14 Task 2

--- assignments/Homework_14/Homework_14.py
+++ assignments/Homework_14/Homework_14.py
@@ -23,3 +23,29 @@ def square_all(*args):
 # Проверка
 add(4, 5)
 square_all(1, 2, 3)
+
+#Task2
+
+def stop_words(words: list):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+
+            for word in words:
+                result = result.replace(word, "*")
+
+            return result
+
+        return wrapper
+    return decorator
+
+
+@stop_words(["pepsi", "BMW"])
+def create_slogan(name: str) -> str:
+    return f"{name} пьет pepsi в своем новеньком BMW!"
+
+
+# Проверка
+assert create_slogan("Steve") == "Steve пьет * в своем новеньком *!"
+
+print(create_slogan("Steve"))
