829e4c3572bd81bd11ce4b1f5a9a0019ee1ce0d4
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-06-10 15:33:57 +0300

    выполнение домашнего задания №12 Task 2

--- assignments/Homework_12/Homework_12.py
+++ assignments/Homework_12/Homework_12.py
@@ -9,3 +9,18 @@ def my_function():
 
 print("Количество локальных переменных:",
       my_function.__code__.co_nlocals)
+
+#Task2
+
+def outer_function(x):
+    def inner_function(y):
+        return x + y  # использует переменную из внешней функции
+    return inner_function  # возвращаем функцию
+
+# создаём функцию-результат
+add_five = outer_function(5)
+
+# вызываем внутреннюю функцию
+result = add_five(10)
+
+print(result)  # 15
