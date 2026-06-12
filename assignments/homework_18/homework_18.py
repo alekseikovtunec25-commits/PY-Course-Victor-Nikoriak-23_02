1cfb6f74be1acf7ff224f482902c04aa1a519de5
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-06-12 21:18:28 +0300

    Выполнение домашнего задания №18 Task 3

--- assignments/homework_18/homework_18.py
+++ assignments/homework_18/homework_18.py
@@ -85,3 +85,73 @@ class Worker:
 
         if self not in value.workers:
             value.add_worker(self)
+
+#Task 3
+
+from functools import wraps
+
+
+class TypeDecorators:
+
+    @staticmethod
+    def to_int(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return int(func(*args, **kwargs))
+
+        return wrapper
+
+    @staticmethod
+    def to_str(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return str(func(*args, **kwargs))
+
+        return wrapper
+
+    @staticmethod
+    def to_bool(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+
+            if isinstance(result, str):
+                return result.lower() == "true"
+
+            return bool(result)
+
+        return wrapper
+
+    @staticmethod
+    def to_float(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return float(func(*args, **kwargs))
+
+        return wrapper
+# Выполянем проверку
+@TypeDecorators.to_int
+def do_nothing(string):
+    return string
+
+
+@TypeDecorators.to_bool
+def do_something(string):
+    return string
+
+
+@TypeDecorators.to_float
+def get_price():
+    return "99.99"
+
+
+@TypeDecorators.to_str
+def get_number():
+    return 100
+
+print(do_nothing("25"))
+print(type(do_nothing("25")))
