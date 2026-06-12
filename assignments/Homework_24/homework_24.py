aa2ee0c022a36c136e4631383893f2a03dba6707
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-06-13 00:32:45 +0300

    Выполнение домашнего задания №24 Task 2

--- assignments/Homework_24/homework_24.py
+++ assignments/Homework_24/homework_24.py
@@ -1,3 +1,5 @@
+#Task1
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -33,3 +35,61 @@ if __name__ == "__main__":
     text = input("Введите последовательность символов: ")
     result = reverse_sequence(text)
     print("Результат:", result)
+
+#Task2
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.items[-1]
+
+
+def is_balanced(sequence: str) -> bool:
+    stack = Stack()
+
+    pairs = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for char in sequence:
+        # если открывающая скобка → кладём в стек
+        if char in "([{":
+            stack.push(char)
+
+        # если закрывающая → проверяем соответствие
+        elif char in ")]}":
+            if stack.is_empty():
+                return False
+
+            top = stack.pop()
+            if top != pairs[char]:
+                return False
+
+    # если стек пуст → всё сбалансировано
+    return stack.is_empty()
+
+
+if __name__ == "__main__":
+    text = input("Введите последовательность: ")
+
+    if is_balanced(text):
+        print("Скобки сбалансированы ✅")
+    else:
+        print("Скобки НЕ сбалансированы ❌")
