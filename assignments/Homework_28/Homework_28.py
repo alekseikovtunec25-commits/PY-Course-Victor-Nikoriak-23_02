734ed175fd1c4f0639daf2e771178e3b579f1311
alekseikovtunec25-commits <alekseikovtunec25gmail.com>
2026-06-15 01:47:14 +0300

    Выполнение домашнего задания №28 Task2

--- assignments/Homework_28/Homework_28.py
+++ assignments/Homework_28/Homework_28.py
@@ -30,3 +30,52 @@ def cocktail_sort(arr):
         start += 1
 
     return arr
+
+#Task 2
+def merge(arr, left, mid, right):
+    # создаём временные массивы вручную
+    left_part = []
+    right_part = []
+
+    # копируем левую часть
+    for i in range(left, mid + 1):
+        left_part.append(arr[i])
+
+    # копируем правую часть
+    for i in range(mid + 1, right + 1):
+        right_part.append(arr[i])
+
+    i = j = 0
+    k = left
+
+    # слияние двух частей
+    while i < len(left_part) and j < len(right_part):
+        if left_part[i] <= right_part[j]:
+            arr[k] = left_part[i]
+            i += 1
+        else:
+            arr[k] = right_part[j]
+            j += 1
+        k += 1
+
+    # остатки
+    while i < len(left_part):
+        arr[k] = left_part[i]
+        i += 1
+        k += 1
+
+    while j < len(right_part):
+        arr[k] = right_part[j]
+        j += 1
+        k += 1
+
+def merge_sort(arr, left, right):
+    if left >= right:
+        return
+
+    mid = (left + right) // 2
+
+    merge_sort(arr, left, mid)
+    merge_sort(arr, mid + 1, right)
+
+    merge(arr, left, mid, right)
